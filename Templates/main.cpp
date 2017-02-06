a4669ca5b255ac2dfe3d115e6c6afd6fa5c2448a
alex-t <alex-t@tiger.lan>
2017-02-07 01:10:28 +0300

    additional experiment :)

--- Templates/main.cpp
+++ Templates/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-template<typename T>
+template<typename T, typename U>
 class Vector {
 private:
 	T* elem; // elem points to an array of sz elements of type T
@@ -16,22 +16,31 @@ public:
 	T& operator[](int i);
 	const T& operator[](int i) const;
 	int size() const { return sz; }
+	
+	T& SomeFunc();
 };
 
-template<typename T>
-Vector<T>::Vector(int s)
+template<typename T, typename U>
+Vector<T,U>::Vector(int s)
 {
 	if (s<0) throw 1;
 	elem = new T[s];
 	sz = s;
 }
 
-template<typename T>
-const T& Vector<T>::operator[](int i) const
+template<typename T, typename U>
+const T& Vector<T,U>::operator[](int i) const
 {
 	if (i<0 || size()<=i)
 		throw out_of_range{"Vector::operator[]"};
 	return elem[i];
+}
+
+template<typename T,typename U>
+T& Vector<T,U>::SomeFunc()
+{
+	T t;
+	return t;
 }
 
 int main(int argc, char **argv)
